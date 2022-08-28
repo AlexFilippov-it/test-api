4c66da34ea0abe6baa34cdd5bd448284a6963ff6
Саша Филиппов <apple@MacBook-Pro.local>
2022-08-28 19:28:08 +0300

    edit httpbin

--- httpbin/test_api_httpbin.py
+++ httpbin/test_api_httpbin.py
@@ -3,17 +3,9 @@ import pytest
 
 # Check request status code
 @pytest.mark.regres
-@pytest.mark.parametrize("code", [200])
+@pytest.mark.parametrize("code", [200, 300, 404, 500])
 def test_url_status_200(base_url, code, request_method):
-    target = base_url + "200"
-    response = request_method(url=target)
-    assert response.status_code == code
-
-
-@pytest.mark.regres
-@pytest.mark.parametrize("code", [300])
-def test_url_status_300(base_url, code, request_method):
-    target = base_url + "300"
+    target = base_url + f"{code}"
     response = request_method(url=target)
     assert response.status_code == code
 
@@ -22,21 +14,5 @@ def test_url_status_300(base_url, code, request_method):
 @pytest.mark.parametrize("code", [400])
 def test_url_status_400(base_url, code, request_method):
     target = base_url + "uyuyuyuyuyuyu"
-    response = request_method(url=target)
-    assert response.status_code == code
-
-
-@pytest.mark.regres
-@pytest.mark.parametrize("code", [404])
-def test_url_status_404(base_url, code, request_method):
-    target = base_url + "404"
-    response = request_method(url=target)
-    assert response.status_code == code
-
-
-@pytest.mark.regres
-@pytest.mark.parametrize("code", [500])
-def test_url_status_500(base_url, code, request_method):
-    target = base_url + "500"
     response = request_method(url=target)
     assert response.status_code == code
